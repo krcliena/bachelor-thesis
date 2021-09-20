69a9dd2af23bad005f26043a9cdd23497906c5a6
krcliena <74600089+krcliena@users.noreply.github.com>
2021-09-21 01:16:40 +0200

    Update disparate_impact_score.py

--- fairML/algorithms/inprocessing/disparate_impact_score.py
+++ fairML/algorithms/inprocessing/disparate_impact_score.py
@@ -12,12 +12,20 @@ class DisparateImpactScore():
         return None
     
     def fit(self, clstype, datatype = None, const_type = None, dataset = None, sensitive = None):
+    """ Parameters
+        ----------
+        sensitive (str): Name of sensitive attribute if dataset includes multiple sensitive attributes. If not, None is given.
+        clstype (str): 'lin-svm' for SVM classifier or 'log-reg' for Logistic Regression
+        dataset (dataset_obj): One of the five dataset objects.
+        """
         
         self.w = main.execute_multiple(clstype, iterations=1, const_type=const_type, 
                                        datatype = datatype, dataset = dataset, sensitive = sensitive)
         return self
     
     def predict(self, X_test):
+    """ Returns predicted outcome of fairness-enhanced classifier."""
+ 
         X_test = ut.add_intercept(X_test)
         y_pred = np.sign(np.dot(X_test, self.w))
         y_pred = pd.Series(y_pred).replace({-1: 0})
