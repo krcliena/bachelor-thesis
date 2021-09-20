221c7c183f9f92c7b47561d9839ad03c5822442b
krcliena <74600089+krcliena@users.noreply.github.com>
2021-09-21 01:03:00 +0200

    Update reweighing.py

--- fairML/algorithms/preprocessing/reweighing.py
+++ fairML/algorithms/preprocessing/reweighing.py
@@ -2,9 +2,19 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 class Reweighing():
-    
+"""References:
+        Kamiran, Faisal, and Toon Calders. "Data preprocessing techniques 
+        for classification without discrimination." Knowledge and Information Systems 33.1 (2012):
+    """
+
     def __init__(self, X_train: np.ndarray, y_train: np.ndarray, S_train: np.ndarray,
                  sens_group_name, non_sens_group_name, clf = LogisticRegression(max_iter = 1000)):
+    """Parameters
+        ----------
+            sens_group_name: Name of privileged group 
+            non_sens_group_name: Name of unprivileged group
+            clf: Classifier to use for training, default is LogisticRegression()
+        """
         self.X_train = X_train
         self.y_train = y_train
         self.S_train = S_train
@@ -18,6 +28,12 @@ class Reweighing():
         self.sens_numerical_arr = sens_numerical_arr
         
     def reweighing(self):
+    """Calculate weights for each sensitive attribute value and class label combination
+    
+        Returns
+        -------
+        weights (list): Weights matrix for each combination
+        """
         n = len(self.sens_numerical_arr)
         weights = [0 for _ in range(n)]
         S = [0, 0]
@@ -43,8 +59,10 @@ class Reweighing():
     
         return W, S_C, weights
     
-    #classifier is LogisticRegression, user must be able to choose as they wish
     def fit(self):
+    """
+        Use calculated weights to train. Must include classifier which supports weights.
+        """
         self.W, self.S_C, self.weights = self.reweighing()
         weights = self.weights
         #Is that correct?
@@ -52,6 +70,11 @@ class Reweighing():
         return self
     
     def predict(self, X_test):
+      """
+        Returns
+        -------
+        y_pred: Predicted outcomes after doing fairness-enhancment
+        """
         y_pred = self.clf.predict(X_test)
         return y_pred
     
