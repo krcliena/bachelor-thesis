9764d27182798bf95497329ea574c532fe31f41b
krcliena <74600089+krcliena@users.noreply.github.com>
2021-09-21 00:37:59 +0200

    Update ricci_obj.py

--- fairML/data/objects/ricci_obj.py
+++ fairML/data/objects/ricci_obj.py
@@ -50,6 +50,9 @@ class RicciData():
     #5. Split data into training and testing data.
     def train_test_split(self, train_size = 0.8, val = None, sensitive = None):
      """Serves as a wrapper around Scikit-learn's train_test_split function. Returns the train and test splits on the data.
+        
+        Parameters
+        ----------
         'sensitive': Choose which sensitive attribute to split on, since only one exits, default is None.
         Returns
         -------
