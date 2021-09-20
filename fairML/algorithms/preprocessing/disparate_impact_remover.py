4cf45b4e0d8397d36de7ecc7af2378681195e56d
krcliena <74600089+krcliena@users.noreply.github.com>
2021-09-21 00:45:05 +0200

    Update disparate_impact_remover.py

--- fairML/algorithms/preprocessing/disparate_impact_remover.py
+++ fairML/algorithms/preprocessing/disparate_impact_remover.py
@@ -2,11 +2,28 @@ from BlackBoxAuditing.repairers.GeneralRepairer import Repairer
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-#Partially taken from AIF360
+#Partially taken from AIF360 under the Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+#Corresponding repository https://github.com/Trusted-AI/AIF360/tree/master/aif360
+
 
 class DisparateImpactRemover():
+"""References:
+        .. [1] M. Feldman, S. A. Friedler, J. Moeller, C. Scheidegger, and
+           S. Venkatasubramanian, "Certifying and removing disparate impact."
+           ACM SIGKDD International Conference on Knowledge Discovery and Data
+           Mining, 2015.
+    """
     
     def __init__(self, X_train, X_test, y_train, y_test, sens_attr_name,  clf = LogisticRegression(max_iter=1000)):
+    """
+        Parameters
+        ----------
+            repair_level (float): Repair amount. 0.0 is no repair while 1.0 is
+                full repair.
+            sens_attr_name (str): Single sensitive attribute with which to
+                do repair.
+            clf: Classifier to use for training, default is LogisticRegression()
+        """
         self.columns = X_train.columns
         self.X_train = X_train.to_numpy().tolist().copy()
         self.y_train = y_train.copy()
@@ -26,6 +43,9 @@ class DisparateImpactRemover():
         return repaired_data_df
     
     def fit(self, repair_level = 1.0, kdd=False):
+    """
+        Repair data then train on the repaired data using classifier clf. 
+        """
         self.repaired_train = self.repair_data(self.X_train,repair_level=repair_level, kdd=kdd).drop(self.sens_index, axis=1)
         self.repaired_test = self.repair_data(self.X_test, repair_level=repair_level, kdd=kdd).drop(self.sens_index, axis=1)
         #drop sensitive attribute before fitting
@@ -34,6 +54,11 @@ class DisparateImpactRemover():
         return self
     
     def predict(self):
+    """
+        Returns
+        -------
+        y_pred: Predicted outcomes after doing fairness-enhancment
+        """
         X_test = self.repaired_test
         y_pred = self.clf.predict(X_test)
         return y_pred
